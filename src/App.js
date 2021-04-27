91d8b580f706754f5b659f8d86b4d176f1270842
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-04-27 17:47:57 -0400

    added todolist

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import React, { useState } from "react";
 import "./App.css";
-import Form from "./components/Form"
-import TodoList from './components/TodoList'
-
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
-    <header>
-      <h1>Fun things to-do list</h1>
-    </header>
-    <Form/>
-    <TodoList/>
+      <header>
+        <h1>Fun things to-do list</h1>
+      </header>
+      <Form todos={todos} inputText={inputText} setTodos={setTodos} setInputText={setInputText}/>
+      <TodoList todos={todos} inputText={inputText}/>
     </div>
   );
 }
